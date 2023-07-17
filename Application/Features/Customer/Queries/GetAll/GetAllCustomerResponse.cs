85c46a43818bce154c9128e553e5d82fd60acd61
dang-hoan <nguyenthedanghoan@gmail.com>
2023-07-17 10:36:19 +0700

    [feature/GetAllCustomers-BBS_15] Get all customers

---
+++ Application/Features/Customer/Queries/GetAll/GetAllCustomerResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.Features.Cusomter.Queries.GetAll
+{
+    public class GetAllCustomerResponse
+    {
+        public long Id { get; set; }
+        public string CustomerName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string? Address { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public decimal? TotalMoney { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime? LastModifiedOn { get; set; }
+    }
+}
