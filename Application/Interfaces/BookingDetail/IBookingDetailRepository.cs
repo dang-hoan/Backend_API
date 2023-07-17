af28863c7c2c5359f835e61651113ba2f3dd2f5f
= <=>
2023-07-18 00:18:00 +0700

    [BBS-23] Add Booking

--- Application/Interfaces/BookingDetail/IBookingDetailRepository.cs
+++ Application/Interfaces/BookingDetail/IBookingDetailRepository.cs
@@ -1,13 +1,8 @@
 ﻿using Application.Interfaces.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Interfaces.BookingDetail
 {
-    public interface IBookingDetailRepository : IRepositoryAsync<Domain.Entities.BookingDetail.BookingDetail,long>
+    public interface IBookingDetailRepository : IRepositoryAsync<Domain.Entities.BookingDetail.BookingDetail, long>
     {
     }
 }
