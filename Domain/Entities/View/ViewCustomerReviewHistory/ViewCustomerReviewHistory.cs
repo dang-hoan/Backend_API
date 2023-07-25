18a1ca97734ea6e68ca0c714a990dbef73153068
dang-hoan <nguyenthedanghoan@gmail.com>
2023-07-25 10:01:26 +0700

    [fix/fix_bug_return_image] Add return image and video link for view feedback history api

--- Domain/Entities/View/ViewCustomerReviewHistory/ViewCustomerReviewHistory.cs
+++ Domain/Entities/View/ViewCustomerReviewHistory/ViewCustomerReviewHistory.cs
@@ -26,13 +26,13 @@ namespace Domain.Entities.View.ViewCustomerReviewHistory
         [Column("feedback_id",TypeName = "bigInt")]
         public long FeedbackId { get; set; }
         [Column("feedback_title",TypeName = "nvarchar")]
-        public string FeedbackTitle { get; set; }
+        public string? FeedbackTitle { get; set; }
         [Column("feedback_service_content",TypeName = "nvarchar")]
-        public string FeedbackServiceContent { get; set; }
+        public string? FeedbackServiceContent { get; set; }
         [Column("feedback_staff_content",TypeName = "nvarchar")]
-        public string FeedbackStaffContent { get; set; }
+        public string? FeedbackStaffContent { get; set; }
         [Column("reply_id",TypeName = "bigInt")]
-        public long ReplyId { get; set; }
+        public long? ReplyId { get; set; }
         [Column("create_on_feedback",TypeName = "datetime")]
         public DateTime CreateOnFeedback { get; set; }
         [Column("rating", TypeName = "smallInt")]
